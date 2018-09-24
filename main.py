4e3a98712c1d43f4c1df0e911c04ee53907518d3
patteb <patteb@gmx.de>
2018-09-24 22:21:54 +0200

    merged

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 # coding=utf-8
 
 
+from time import sleep
+
 import cardbot_serial
 import matching
 import setup
@@ -10,11 +12,8 @@ from scryfall import scryfall
 query, config, ser_if = setup.init()
 
 img_files = scryfall(config.url_pre, query)  # API-call, download images
-cv_img = matching.card_query(img_files)  # imread() downloaded images
-
-# extracting features of all found cards ("ref" as in reference)
-print("Extracting features of references...")
-kp_ref, des_ref = matching.ref_features(cv_img)
+cv_img = matching.ref_prepare(img_files)  # prepare downloaded images
+des_ref = matching.ref_features(cv_img)
 
 setup.remove_temp(config.path)  # cleanup, delete card images
 
@@ -25,12 +24,17 @@ while True:
     print("Feeding next card."),
     if cardbot_serial.feed(ser_if):
         # Resolving the recorded image !!SIMULATED!!! drop "_dummy" 4 da real shit
-        print("Finding features of card."),
-        kp_cam, des_cam = matching.cam_features_dummy("cam.jpg", config.cam_if)
-
+        sleep(1)
+        print("Taking Picture."),
+        cam_img = matching.cam_prepare(config.cam_if)
+        print("Extracting features."),
+        des_cam = matching.cam_features(cam_img)
         print("Matching."),
         img_match = matching.card_matching(des_ref, des_cam)
-        print("Max. score is " + str(img_match) + "."),
+        print("Score is %s." % img_match),
+        if img_match > config.likely_match:
+            matches_found += 1
+            print ("Match #%s found!" % matches_found),
     else:
         print "\nFeeding Error! Exiting."
         quit()
@@ -38,11 +42,11 @@ while True:
     print("Sorting...")
     if cardbot_serial.sort(img_match > config.likely_match, ser_if):
         if (img_match > config.likely_match) and (config.max_matches is not None):
-            matches_found += 1
-            if (matches_found >= config.max_matches):
-                print ("Maximum matching cards found. " + str(matches_found) + " matching cards found. Exiting.")
+            if matches_found >= config.max_matches:
+                print ("Maximum matching cards found. %s matching cards found. Exiting." % matches_found)
                 quit()
         continue
+        sleep(1)
     else:
         print "Sorting Error! Exiting"
         quit()
