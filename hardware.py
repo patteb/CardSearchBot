43f8ae4d16509ff60bcccf7dc4f228315962644c
Patrick <pattebiermann@gmail.com>
2019-06-10 16:28:59 +0200

    bugfix

--- hardware.py
+++ hardware.py
@@ -56,7 +56,7 @@ class ADC:
     CONTROL = 0x00
 
     def __init__(self, config, bus):
-        self.adr = hex(config.ADC) #probably 0x48
+        self.adr = hex(config.ADC)  # probably 0x48
         self.thresh = int(config.thresh)
         self.i2c = bus
         self.i2c.write_byte_data(self.adr, self.CONTROL, 0x00)
@@ -65,16 +65,16 @@ class ADC:
         sensor_value = 0.0
         self.i2c.read_byte_data(self.adr, 0)
         for i in range(0, 10):
-            sleep (0.01)
+            sleep(0.01)
             sensor_value += self.i2c.read_byte_data(self.adr, 0)
         sensor_value = sensor_value / 10
-        return True if sensor_value <= self.thresh else return False
+        return True if sensor_value <= self.thresh else False
 
 
 class Servo:
     # TODO:register etc
     def __init__(self, config, bus, SortOrFeed):
-        self.adr = hex(config.Servo) #probably 0x40
+        self.adr = hex(config.Servo)  # probably 0x40
         self.i2c = bus
         self.speed = int(config.servo_speed)
 
@@ -88,4 +88,4 @@ class Servo:
 
     def write(self, value):
         pass
-    #TODO write to servo
+    # TODO write to servo
