3952b695afacc1eb5529a26d733c8cbb209a949f
Patrick <pattebiermann@gmail.com>
2019-06-12 23:51:30 +0200

    implemented PCA9685 PWM control

--- hardware.py
+++ hardware.py
@@ -72,7 +72,19 @@ class ADC:
 
 
 class Servo:
-    # TODO:register etc
+    """PCA9685 PWM Controller Servo Object. See datasheet @ https://www.nxp.com/docs/en/data-sheet/PCA9685.pdf"""
+    freq = 50
+
+    # registers
+    MODE1 = 0x00
+    MODE2 = 0x01
+    SUBADR1 = 0x02
+    SUBADR2 = 0x03
+    SUBADR3 = 0x04
+    ALLCALLADR = 0x05
+    PRE_SCALE = 0xFE
+    TestMode = 0xFF
+
     def __init__(self, config, bus, SortOrFeed):
         self.adr = hex(config.Servo)  # probably 0x40
         self.i2c = bus
@@ -83,9 +95,37 @@ class Servo:
         else:
             self.channel = int(config.feed_pin)
 
+        # initialize channel register addresses (as to not do that manually...)
+        # e.g: Channel["ON"]["LOW"] = 0xf2
+        Reg_On = {"LOW": 0x06 + 4 * self.channel, "HIGH": 0x07 + 4 * self.channel}
+        Reg_Off = {"LOW": 0x08 + 4 * self.channel, "HIGH": 0x09 + 4 * self.channel}
+        self.Channel = ({"ON": Reg_On, "OFF": Reg_Off})
+
+        # initialize control registers (pgs. 14 & 16 for reference. May need some testing in MODE2)
+        self.i2c.write_byte_data(self.adr, self.MODE1, 0x00)
+        self.i2c.write_byte_data(self.adr, self.MODE1, 0x1C)
+
+        # set default servo pwm frequency
+        self.i2c.write_byte_data(self.adr, self.PRE_SCALE, 0x79)  # 50Hz (20ms) @ 25MHz internal oscillator
+
     def set_speed(self, speed):
         self.speed = speed
 
-    def write(self, value):
-        pass
-    # TODO write to servo
+    def set_pwm_freq(self, freq):
+        """Setting PWM period to given frequency. See pg.25 of PCA9685 Datasheet
+         :param freq: """
+        value = int((25000000 / (4096.0 * freq)) - 1)
+        self.i2c.write_byte_data(self.adr, self.PRE_SCALE, value)
+
+    def set_angle(self, deg):
+        """set servo to a angle
+        :param deg: Angle 0..180deg"""
+
+        duty = int((deg / 180.0) * 4096)
+        duty_low = duty & 0x00FF
+        duty_high = (duty & 0xFF00) >> 8
+
+        self.i2c.write_byte_data(self.adr, self.Channel["ON"]["LOW"], 0x00)
+        self.i2c.write_byte_data(self.adr, self.Channel["ON"]["HIGH"], 0x00)
+        self.i2c.write_byte_data(self.adr, self.Channel["OFF"]["LOW"], duty_low)
+        self.i2c.write_byte_data(self.adr, self.Channel["OFF"]["HIGH"], duty_high)
